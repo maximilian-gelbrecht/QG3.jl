99a499f55bfdc2a31e3232ff265c7234038fd488
Max <maximilian.gelbrecht@posteo.de>
2021-11-21 17:23:35 +0100

    minor fix

--- test/basic_test_gpu.jl
+++ test/basic_test_gpu.jl
@@ -26,22 +26,19 @@ if CUDA.functional()
 
     mean(abs.(QG3.SHtoGrid_dλ(ψ_0_gpu, qg3p_gpu) - togpu(QG3.SHtoGrid_dλ(ψ_0, qg3p_cpu)))) < 1e-10
 
-    J(ψ_0_gpu, q_0_gpu, qg3p_gpu) - QG3.reorder_SH_gpu(J(ψ_0, q_0, qg3p_cpu))
-
-    D(ψ_0_gpu, q_0_gpu, qg3p_gpu) - QG3.reorder_SH_gpu(D(ψ_0, q_0, qg3p_cpu))
-
+    mean(abs.(transform_grid(J(ψ_0_gpu, q_0_gpu, qg3p_gpu),qg3p_gpu) - togpu(transform_grid(J(ψ_0, q_0, qg3p_cpu),qg3p_cpu)))) < 1e-10
 
     A = QG3.QG3MM_gpu(q_0_gpu, [qg3p_gpu, S_gpu], 0.)
 
     B = QG3.QG3MM_base(q_0, [qg3p_cpu, S], 0.)
 
-    mean(abs.(A - B)) < 1e-10
-    # time step
+    @test mean(abs.(A - QG3.reorder_SH_gpu(B,qg3p_cpu.p))) < 1e-10    # time step
     DT = 2π/144
     t_end = 200.
 
     # problem definition with GPU model from the library
-    prob = ODEProblem(QG3.QG3MM_gpu, q_0, (0.,t_end), [qg3p, S])
+    prob = ODEProblem(QG3.QG3MM_gpu, q_0_gpu, (0.,t_end), [qg3p_gpu, S_gpu])
+
     sol = @time solve(prob, AB5(), dt=DT)
 
     @test sol.retcode==:Success
