88e40589bca45f035cce3ffc0d1d5a47b304c4ab
Max <maximilian.gelbrecht@posteo.de>
2022-02-03 20:14:33 +0100

    fix in grid definition with new plans

--- src/basic_types.jl
+++ src/basic_types.jl
@@ -215,7 +215,7 @@ function grid(p::QG3ModelParameters{T}, gridtype::String) where T<:Number
             iFT = plan_cuir2r(FT*(A_real[1,:,:]), p.N_lons, 2)
 
             FT_3d = plan_cur2r(A_real, 3)
-            iFT_3d = plan_cuir2r(FT*A_real, p.N_lons, 3)
+            iFT_3d = plan_cuir2r(FT_3d*A_real, p.N_lons, 3)
 
             truncate_array = nothing
         else
