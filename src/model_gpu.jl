c9208423735852a39697f4fbe440a35dcd61e75e
Maximilian Gelbrecht <maxgelbr@pik-potsdam.de>
2023-01-11 10:28:26 +0100

    doc strings and Jprime

--- src/model_gpu.jl
+++ src/model_gpu.jl
@@ -1,27 +1,45 @@
 # this file offers a full matrix / 3d formulation of the model that is geared toward efficient GPU usage, some more general notes of the implementation are given in the model.jl file
 
 """
-Jacobian in 3D Field formulation (e.g. for GPU), the second term in the q derivatives is the contribution of the f(1+h/H_0) term to the 850hPa level.
+    J(ψ::AbstractArray{T,3}, q::AbstractArray{T,3}, m::QG3Model{T})
+
+Computes the Jacobian for the QG3 model in 3D Field formulation (e.g. for GPU). Takes as an imput the streamfunction ψ and the potential vorticity q' without the coriolis contribution. Adds the contribution of the Coriolis force and orography at the 850hPa level that is prescribed in the MM QG3 Model. 
 """
 J(ψ::AbstractArray{T,3}, q::AbstractArray{T,3}, m::QG3Model{T}) where T<:Number =  transform_SH(SHtoGrid_dμ(ψ, m).*SHtoGrid_dλ(q + m.f_J3, m) - (SHtoGrid_dλ(ψ, m).*SHtoGrid_dμ(q + m.f_J3, m)), m) - SHtoSH_dλ(ψ, m)
 
 """
-Horizontal diffusion, q' is anomolous pv (without coriolis) 3D Fields
+    Jprime(ψ::AbstractArray{T,3}, q::AbstractArray{T,3}, m::QG3Model{T})
+
+Computes the Jacobian (∂ψ/∂μ ∂q/∂λ - ∂ψ/∂λ ∂q/∂μ). Directly computes the derivatives of the two inputs ψ and q and doesn't account for any additional contribution (e.g. from the Coriolis force).
+"""
+Jprime(ψ::AbstractArray{T,3}, q::AbstractArray{T,3}, m::QG3Model{T}) where T<:Number =  transform_SH(SHtoGrid_dμ(ψ, m).*SHtoGrid_dλ(q + m.f_J3, m) - (SHtoGrid_dλ(ψ, m).*SHtoGrid_dμ(q + m.f_J3, m)), m) - SHtoSH_dλ(ψ, m) 
+
+
+"""
+    H(qprime::AbstractArray{T,3}, m::QG3Model{T})
+
+Computes the horizontal diffusion, q' is anomolous pv (without coriolis) 3D Fields
 """
 H(qprime::AbstractArray{T,3}, m::QG3Model{T}) where T<: Number = cH∇8(qprime, m)
 
 """
-Temperature relaxation of all levels
+    TR(ψ::AbstractArray{T,3}, m::QG3Model{T})
+
+Computes the temperature relaxation of all levels
 """
 TR(ψ::AbstractArray{T,3}, m::QG3Model{T}) where T<:Number = reshape(batched_vec(m.TR_matrix, reshape(ψ,3,:)),3 , m.g.size_SH...) 
 
 """
-Dissipiation of all levels, 850hPa has additional Ekman dissipation
+    D(ψ::AbstractArray{T,3}, q::AbstractArray{T,3}, m::QG3Model{T})
+
+Computes the dissipiation of all levels, 850hPa has additional Ekman dissipation.
 """
 D(ψ::AbstractArray{T,3}, q::AbstractArray{T,3}, m::QG3Model{T}) where T<:Number = add_to_level(TR(ψ, m) + H(q, m), EK(ψ[3,:,:], m), 3)
 
 """
-Differentiable and GPU compatible way of adding only to one of the levels of the model
+    add_to_level(ψ::AbstractArray{T,3}, ψ_i::AbstractArray{T,2}, i::Integer)
+
+Differentiable and GPU compatible way of adding only to one of the levels of the model.
 """
 function add_to_level(ψ::AbstractArray{T,3}, ψ_i::AbstractArray{T,2}, i::Integer) where T<:Number
     ψ[i,:,:] += ψ_i
