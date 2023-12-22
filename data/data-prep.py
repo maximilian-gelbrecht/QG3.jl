08d82ed89812fcb59be80f90a5dcfd193c26a3e2
Max <maximilian.gelbrecht@posteo.de>
2023-12-22 11:54:51 +0100

    data-prep update

--- data/data-prep.py
+++ data/data-prep.py
@@ -5,7 +5,7 @@
 # for i in $(ls); do cdo remapbil,n32 ${i} regrid/${i}-t42.nc; done
 #
 # then get a interative job on the hpc for this script (it needs more than 4GB RAM): 
-# srun --qos=priority --ntasks-per-node=14 --pty bash 
+# srun --qos=priority --ntasks-per-node=16 --pty bash 
 #
 # merge final result with cdo 
 # cdo mergetime *nc outfile.nc
@@ -23,8 +23,7 @@ V_DIR = 'v_component_of_wind/hourly/'
 U_FILENAME_BASE = 'ERA5-u-200500800hPa-'
 V_FILENAME_BASE = 'ERA5-v-200500800hPa-'
 PSI_FILENAME_BASE = 'ERA5-sf-200500800hPa-'
-#years = np.concatenate((range(79,100,range(00,21)))
-years = np.concatenate((range(99,100),range(00,21)))
+years = np.concatenate((range(79,100),range(00,21)))
 
 for iyear in years: 
     # Read u and v wind components from file.
@@ -37,4 +36,4 @@ for iyear in years:
     # Call methods to compute streamfunction and relative vorticity.
     psi = w.streamfunction()
 
-    iris.save(psi, SAVE_DIR+PSI_FILENAME_BASE+str(iyear)+'.nc')
+    iris.save(psi, SAVE_DIR+PSI_FILENAME_BASE+f'{iyear:02d}'+'.nc')
